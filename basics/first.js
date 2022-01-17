d57de39b914d705732a89b98f9f7fee4d7e6b1b3
Jak315 <97089275+Jak315@users.noreply.github.com>
2022-01-17 17:23:57 +0800

    Add files via upload

---
+++ basics/first.js
@@ -0,0 +1,46 @@
+// //  //s let age = 30;
+// // s console.log(age);
+// // //s
+// // var 
+// let x = 1;
+// let y = 5;
+// let z = 16;
+// let c = 2;
+
+// let answer = ((x+y**2)-z*(x/c));
+
+// alert(answer);
+
+// alert("(x+y**2)-z*(x/c) = " + answer);
+
+// let x = 129
+// let y = 7
+
+// let answer = x%y ;
+
+// alert("x%y=" + answer);
+
+// let a = prompt("ehnii too x= ");
+// let b = prompt("suuliin too y= ");
+
+// let x = parseInt(a);
+// let y = parseInt(b);
+
+// let answer1 = ( x + y );
+
+// // alert (x + "+" + y + "=" + answer1);
+
+// alert("( x + y )=" +answer1)
+
+
+
+let x = prompt("ehnii too");
+let y = prompt("suuliin too");
+
+let a = parseInt(x);
+let b = parseInt(y);
+
+let answer = a + b;
+
+alert( a + "+" + b + "=" + answer );
+
---
+++ basics/function.js
@@ -0,0 +1,12 @@
+
+let x = 2022;
+
+let name = prompt("tanii ner");
+let age = prompt("nas hed ve");
+let ungu = prompt("tanii arisnii ungu yamar ve");
+let huuhed = prompt("ta heden huuhedtei ve");
+
+alert(" hello " + name + " your birth is " + (x-age) + " tanii arisnii ungu " + ungu + " ta heden huuhedtei ve " + huuhed);
+// ene
+let answer = confirm("are u sure")
+alert (answer);
